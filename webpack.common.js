aa2e5e64dd68f0ac7235052ee41d998a739eaec1
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-23 14:31:24 +0900

    html-webpack-plugin の設定追加

--- webpack.common.js
+++ webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	entry: './src/js/main.js',
@@ -6,4 +7,9 @@ module.exports = {
 		path: path.resolve(__dirname, 'public'),
 		filename: 'js/bundle.js',
 	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/html/index.html',
+		}),
+	],
 };
