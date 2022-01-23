f74a4ff469f5cc0d9b7040eab2e7d33a28c99535
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-23 14:21:32 +0900

    terser-webpack-plugin の設定追記

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,6 +1,20 @@
 const { merge } = require('webpack-merge');
 const commonConfig = require('./webpack.common');
+// JavaScriptを圧縮するWebpackのプラグイン
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(commonConfig, {
 	mode: 'production',
+	optimization: {
+		minimizer: [
+			new TerserPlugin({
+				extractComments: false,
+				terserOptions: {
+					compress: {
+						drop_console: true,
+					},
+				},
+			}),
+		],
+	},
 });
