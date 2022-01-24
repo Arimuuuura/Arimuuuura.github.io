5283bd036ae2c46e234db424a93ff0cc09dee56f
y-arimura1222 <y.arimura.carecon@gmail.com>
2022-01-24 23:52:57 +0900

    タブの処理を独自モジュールに分けた

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,9 @@
 import '../scss/style.scss';
+import { tab } from './modules/tab';
 
 ('use strict');
 {
-  // タブメニュー
-  const menuItems = document.querySelectorAll('.menu li a');
-  const contents = document.querySelectorAll('.content');
-  menuItems.forEach((clickedItem) => {
-    clickedItem.addEventListener('click', (e) => {
-      e.preventDefault();
-      // a 要素は通常画面遷移するので、e.preventDefault()とすることで画面遷移しなくなる
-      menuItems.forEach((item) => {
-        item.classList.remove('active');
-        // クリックされていない要素から active class を取り除く
-      });
-      clickedItem.classList.add('active');
-      // クリックしたら active 要素を付ける
-      contents.forEach((content) => {
-        content.classList.remove('active');
-        // クリックされていない要素から active class を取り除く
-      });
-      document.getElementById(clickedItem.dataset.id).classList.add('active');
-    });
-  });
+  tab();
 
   // async を付けることで非同期関数と呼ばれるようになる
   // async, await で fetch を使うと Response オブジェクトが帰ってくる
